6a58234a61451f647055fc5f0a3ce8eb77ecb66e
psihozykt <psihozykt@gmail.com>
2022-02-08 22:04:31 +0900

    LoginPage layout

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
+import './LoginPage.css'
+import logo from '../../assets/img/loginPage/logo.svg'
 
 function LoginPage() {
   return (
-    <form className="login-form">
-      <label htmlFor="login">
-        Логин
-        <input type="text" id="login" />
-      </label>
+    <div className="login-page">
+      <img src={logo} alt="circle, rectangle, circle, parallelogram" />
+      <form className="login-form">
+        <div className="login-form__header">API-консолька</div>
+        <label htmlFor="login">
+          Логин
+          <input type="text" id="login" />
+        </label>
 
-      <label htmlFor="sublogin">
-        Сублогин
-        <input type="text" id="sublogin" />
-      </label>
+        <label className="login-form__sublogin" htmlFor="sublogin">
+          Сублогин
+          <span className="login-form__sublogin_optional"> Опционально </span>
+          <input type="text" id="sublogin" />
+        </label>
 
-      <label htmlFor="password">
-        Пароль
-        <input type="text" id="password" />
-      </label>
-
-      <input type="submit" />
-    </form>
+        <label htmlFor="password">
+          Пароль
+          <input type="text" id="password" />
+        </label>
+        <button type="submit" className="login-form__submit">
+          Войти
+        </button>
+      </form>
+      <div className="github-link">
+        https://github.com/PsihoZykt/API-console{' '}
+      </div>
+    </div>
   )
 }
 
