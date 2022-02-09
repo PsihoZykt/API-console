096bc27dae7915a1c3d123affe44360b4415b9e9
psihozykt <psihozykt@gmail.com>
2022-02-09 19:38:53 +0900

    remove react-cookies

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { CookiesProvider } from 'react-cookie'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
