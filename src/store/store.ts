206dba4442fa57891d046c08faa417e870465b48
psihozykt <psihozykt@gmail.com>
2022-02-10 14:24:29 +0900

    added redux-dev-tools

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,15 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import loginReducer from './reducers/loginReducer'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from '@redux-devtools/extension'
 
 const rootReducer = combineReducers({
   loginPage: loginReducer,
   form: formReducer,
 })
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 export default store
