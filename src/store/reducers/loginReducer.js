4c26e16837c9e22b2155a063913c1267970a109f
psihozykt <psihozykt@gmail.com>
2022-02-10 14:49:29 +0900

    Moved action types to constants

--- src/store/reducers/loginReducer.js
+++ src/store/reducers/loginReducer.js
@@ -5,16 +5,18 @@ const initialState = {
   },
   isLoading: false,
 }
+const SET_AUTH_RESULT = 'SET_AUTH_RESULT'
+const SET_IS_LOADING = 'SET_IS_LOADING'
 
 export default function loginReducer(state = initialState, action) {
   switch (action.type) {
-    case 'SET_AUTH_RESULT': {
+    case SET_AUTH_RESULT: {
       return {
         ...state,
         authResult: action.authResult,
       }
     }
-    case 'SET_IS_LOADING': {
+    case SET_IS_LOADING: {
       return {
         ...state,
         isLoading: action.isLoading,
@@ -27,13 +29,13 @@ export default function loginReducer(state = initialState, action) {
 
 export const setIsLoading = (isLoading) => {
   return {
-    type: 'SET_IS_LOADING',
+    type: SET_IS_LOADING,
     isLoading: isLoading,
   }
 }
 export const setAuthResult = (authResult) => {
   return {
-    type: 'SET_AUTH_RESULT',
+    type: SET_AUTH_RESULT,
     authResult: authResult,
   }
 }
