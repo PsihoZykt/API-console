dc18b7c2e3ffb5bd731f17075b1b7d0dc5b5380a
psihozykt <psihozykt@gmail.com>
2022-02-10 22:08:48 +0900

    preparing store for consolePage

---
+++ src/store/selectors/consolePage/selector.js
@@ -0,0 +1,6 @@
+export const getCurrentRequest = (state) => {
+  return state.consolePage.currentRequest
+}
+export const getRequestHistory = (state) => {
+  return state.consolePage.requestHistory
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,9 +3,11 @@ import { reducer as formReducer } from 'redux-form'
 import loginReducer from './reducers/loginReducer'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from '@redux-devtools/extension'
+import consoleReducer from './reducers/consoleReducer'
 
 const rootReducer = combineReducers({
   loginPage: loginReducer,
+  consolePage: consoleReducer,
   form: formReducer,
 })
 
