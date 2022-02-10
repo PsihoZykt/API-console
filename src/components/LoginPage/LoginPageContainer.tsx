a9a6a966318974363d2c359c5b1c41dad1161de8
psihozykt <psihozykt@gmail.com>
2022-02-10 13:19:41 +0900

    login-thunk refactor, simplified mapDispatchToProps

--- src/components/LoginPage/LoginPageContainer.tsx
+++ src/components/LoginPage/LoginPageContainer.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { formValueSelector } from 'redux-form'
 import LoginPage from './LoginPage'
-import { onAuth } from '../../store/thunks/login'
+import { signIn } from '../../store/thunks/login'
 
 const LoginPageContainer = ({
   login,
@@ -12,10 +12,10 @@ const LoginPageContainer = ({
   password,
   isLoading,
   authResult,
-  onAuth,
+  signIn,
 }: any) => {
   const navigate = useNavigate()
-
+  console.log(authResult)
   useEffect(() => {
     if (localStorage.getItem('sendsay_session')) {
       navigate('/console')
@@ -24,34 +24,33 @@ const LoginPageContainer = ({
 
   const submit = (e: any) => {
     e.preventDefault()
-    onAuth(login, sublogin, password).then(() => {
-      if (!authResult.isError) navigate('/console')
-    })
+    signIn(login, sublogin, password)
   }
+  useEffect(() => {
+    if (authResult.res.session) navigate('/console')
+  }, [authResult])
+
   return (
     <LoginPage submit={submit} authResult={authResult} isLoading={isLoading} />
   )
 }
 
 const selector = formValueSelector('login')
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    onAuth: (login: any, sublogin: any, password: any) =>
-      dispatch(onAuth(login, sublogin, password)),
-  }
-}
-export default connect((state: any) => {
-  const login = selector(state, 'login')
-  const sublogin = selector(state, 'sublogin')
-  const password = selector(state, 'password')
-  const isLoading = state.loginPage.isLoading
-  const authResult = state.loginPage.authResult
+export default connect(
+  (state: any) => {
+    const login = selector(state, 'login')
+    const sublogin = selector(state, 'sublogin')
+    const password = selector(state, 'password')
+    const isLoading = state.loginPage.isLoading
+    const authResult = state.loginPage.authResult
 
-  return {
-    login,
-    sublogin,
-    password,
-    isLoading,
-    authResult,
-  }
-}, mapDispatchToProps)(LoginPageContainer)
+    return {
+      login,
+      sublogin,
+      password,
+      isLoading,
+      authResult,
+    }
+  },
+  { signIn }
+)(LoginPageContainer)
--- src/store/thunks/login.js
+++ src/store/thunks/login.js
@@ -1,12 +1,10 @@
 import { setAuthResult, setIsLoading } from '../reducers/loginReducer'
 import { auth } from '../../api/sendsay'
 
-export const onAuth = (login, sublogin, password) => {
-  return (dispatch) => {
-    dispatch(setIsLoading(true))
-    return auth(login, sublogin, password).then((res) => {
-      dispatch(setAuthResult(res))
-      dispatch(setIsLoading(false))
-    })
-  }
+export const signIn = (login, sublogin, password) => (dispatch) => {
+  dispatch(setIsLoading(true))
+  auth(login, sublogin, password).then((res) => {
+    dispatch(setAuthResult(res))
+    dispatch(setIsLoading(false))
+  })
 }
