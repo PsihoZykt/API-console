5b73e94883409a57824a68fa4d5dd5ff204c91aa
psihozykt <psihozykt@gmail.com>
2022-02-12 00:48:01 +0900

    Create Run Request thunk, removed analogical logic from component

--- src/components/ConsolePage/ConsolePageContainer.tsx
+++ src/components/ConsolePage/ConsolePageContainer.tsx
@@ -1,49 +1,51 @@
-import React, {useEffect, useState} from 'react'
-import {connect} from 'react-redux'
+import React, { useEffect } from 'react'
+import { connect, ConnectedProps } from 'react-redux'
 import ConsolePage from './ConsolePage'
 
-import {getCurrentRequest, getRequestHistory,} from 'store/selectors/consolePage/selector'
-import {Request, RequestStatus} from 'store/reducers/consoleReducer'
-import {authWithSession, logout, makeRequest} from 'api/sendsay'
-import {useNavigate} from 'react-router-dom'
-
-import {getAuthResult} from 'store/selectors/loginPage/selector'
-import {AuthResult} from 'store/reducers/loginReducer'
-import {RootState} from 'store/store'
-import {loginActions, SetAuthResultActionType} from "store/actions/login/loginActions";
 import {
-  ChangeCurrentRequestActionType,
-  ChangeRequestBodyActionType,
-  consoleActions,
-  SubmitRequestActionType
-} from "store/actions/console/consoleActions";
+  getCurrentRequest,
+  getIsRequestError,
+  getIsResponseError,
+  getRequestHistory,
+} from 'store/selectors/consolePage/selector'
+import { Request } from 'store/reducers/consoleReducer'
+import { authWithSession, logout } from 'api/sendsay'
+import { useNavigate } from 'react-router-dom'
 
-type MapState = {
-  currentRequest: Request,
-  requestHistory: Array<Request>,
-  auth: AuthResult,
-}
-type DispatchState = {
-  changeRequestBody: (body: string) => ChangeRequestBodyActionType
-  submitRequest: (request: Request) => SubmitRequestActionType
-  changeCurrentRequest: (request: Request) =>
-      ChangeCurrentRequestActionType
-  setAuthResult: (authResult: AuthResult) =>
-      SetAuthResultActionType
-}
-type Props = MapState & DispatchState
+import { getAuthResult } from 'store/selectors/loginPage/selector'
+import { AuthResult } from 'store/reducers/loginReducer'
+import { RootState } from 'store/store'
+import { loginActions } from 'store/actions/login/loginActions'
+import { consoleActions } from 'store/actions/console/consoleActions'
+import { runRequest } from 'store/thunks/consoleThunks'
+
+// type MapState = {
+//     currentRequest: Request,
+//     requestHistory: Array<Request>,
+//     isRequestError: boolean,
+//     isResponseError: boolean
+//     auth: AuthResult,
+// }
+// type DispatchState = {
+//     changeRequestBody: (body: string) => ChangeRequestBodyActionType
+//     changeCurrentRequest: (request: Request) =>
+//         ChangeCurrentRequestActionType
+//     setAuthResult: (authResult: AuthResult) =>
+//         SetAuthResultActionType
+//     runRequest: (body: string) => void
+// }
+type Props = ReduxProps
 
 const ConsolePageContainer = ({
-                                currentRequest,
-                                changeRequestBody,
-                                submitRequest,
-                                requestHistory,
-                                changeCurrentRequest,
-                                auth,
-                                setAuthResult,
-                              }: Props) => {
-  const [isRequestError, setIsRequestError] = useState(false)
-  const [isResponseError, setIsResponseError] = useState(false)
+  currentRequest,
+  isResponseError,
+  isRequestError,
+  changeRequestBody,
+  requestHistory,
+  auth,
+  runRequest,
+  setAuthResult,
+}: Props) => {
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -64,65 +66,43 @@ const ConsolePageContainer = ({
     })
   }
 
-  const onSubmitRequest = (body: string) => {
+  const onSubmitRequest = async (body: string) => {
     console.log(body)
-    let request
-    try {
-      request = JSON.parse(body)
-      console.log(request)
-
-      setIsRequestError(false)
-    } catch (e) {
-      console.log('Parse error')
-      setIsRequestError(true)
-    }
-    if (request) {
-      makeRequest(body).then((res) => {
-        // Sendsay returns _ehid only with successful request, so we can use it
-        const isSuccessful = !!(res._ehid ?? false)
-        const status = isSuccessful
-          ? RequestStatus.Successful
-          : RequestStatus.Unsuccessful
-
-        const id = res['_ehid']
-        const requestText = body
-        const requestResponse = JSON.stringify(res)
-        setIsResponseError(!isSuccessful)
-        changeCurrentRequest({ status, id, requestText, requestResponse })
-        submitRequest({ status, id, requestText, requestResponse })
-      })
-    }
+    await runRequest(body)
   }
+
   return (
-      <ConsolePage
-          onSubmitRequest={onSubmitRequest}
-          currentRequest={currentRequest}
-          changeRequestBody={changeRequestBody}
-          requestHistory={requestHistory}
-          isRequestError={isRequestError}
-          isResponseError={isResponseError}
-          auth={auth}
-          onLogout={onLogout}
-      />
+    <ConsolePage
+      onSubmitRequest={onSubmitRequest}
+      currentRequest={currentRequest}
+      changeRequestBody={changeRequestBody}
+      requestHistory={requestHistory}
+      isRequestError={isRequestError}
+      isResponseError={isResponseError}
+      auth={auth}
+      onLogout={onLogout}
+    />
   )
 }
 
-const connector = connect<MapState, DispatchState, Record<string, never>, RootState>(
-    (state: RootState) => {
-      return {
-        currentRequest: getCurrentRequest(state),
-        requestHistory: getRequestHistory(state),
-        auth: getAuthResult(state),
-      }
-    },
-    {
-      changeRequestBody: (body: string) => consoleActions.createChangeRequestBodyAction(body),
-      submitRequest: (request: Request) => consoleActions.createSubmitRequestAction(request),
-      changeCurrentRequest: (request: Request) =>
-          consoleActions.createChangeCurrentRequestAction(request),
-      setAuthResult: (authResult: AuthResult) =>
-          loginActions.setAuthResultAction(authResult),
-
+const connector = connect(
+  (state: RootState) => {
+    return {
+      currentRequest: getCurrentRequest(state),
+      requestHistory: getRequestHistory(state),
+      isRequestError: getIsRequestError(state),
+      isResponseError: getIsResponseError(state),
+      auth: getAuthResult(state),
     }
+  },
+  {
+    changeRequestBody: (body: string) => consoleActions.changeRequestText(body),
+    changeCurrentRequest: (request: Request) =>
+      consoleActions.changeCurrentRequest(request),
+    setAuthResult: (authResult: AuthResult) =>
+      loginActions.setAuthResultAction(authResult),
+    runRequest,
+  }
 )
+type ReduxProps = ConnectedProps<typeof connector>
 export default connector(ConsolePageContainer)
---
+++ src/store/thunks/consoleThunks.tsx
@@ -0,0 +1,40 @@
+import { makeRequest } from 'api/sendsay'
+import { RequestStatus } from 'store/reducers/consoleReducer'
+import { ThunkAction } from 'redux-thunk'
+import { RootState } from 'store/store'
+import {
+  consoleActions,
+  ConsoleActionsType,
+} from 'store/actions/console/consoleActions'
+
+export const runRequest =
+  (body: string): ThunkAction<void, RootState, unknown, ConsoleActionsType> =>
+  async (dispatch) => {
+    let request
+    console.log('das')
+
+    try {
+      request = JSON.parse(body)
+      console.log(request)
+      dispatch(consoleActions.setIsRequestError(false))
+    } catch (e) {
+      console.log('Parse error')
+      dispatch(consoleActions.setIsRequestError(true))
+    }
+    if (request) {
+      const response = await makeRequest(body)
+      // Sendsay returns _ehid only with successful request, so we can use it
+      const isSuccessful = !!(response._ehid ?? false)
+      const status = isSuccessful
+        ? RequestStatus.Successful
+        : RequestStatus.Unsuccessful
+
+      const id = response['_ehid']
+      const requestText = body
+      const requestResponse = JSON.stringify(response)
+      const newRequest = { status, id, requestText, requestResponse }
+      dispatch(consoleActions.setIsResponseError(!isSuccessful))
+      dispatch(consoleActions.changeCurrentRequest(newRequest))
+      dispatch(consoleActions.addRequestToHistory(newRequest))
+    }
+  }
